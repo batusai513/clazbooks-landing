490a357f1f91c9ab3f6ea688988382fa38c9d455
Hernan Orozco <horozco15@gmail.com>
2019-12-25 10:37:09 -0500

    Fix production issue

--- pages/api/landing_pages/index.js
+++ pages/api/landing_pages/index.js
@@ -5,9 +5,6 @@ export default (req, res) => {
   axios.get('https://mobile.clazbooks.com/api/landing_pages', body).then(
     response => {
       const { headers } = response;
-      Object.keys(headers).forEach(key => {
-        // res.setHeader(key, headers[key]);
-      });
       res.status(response.status).json(response.data);
     },
     err => {
